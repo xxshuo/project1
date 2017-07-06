1e9faaa67d29baa1026113e4efe3bb72f3c33129
root <root@linux4.(none)>
2017-07-06 20:32:06 +0800

    checkfile

---
+++ checkfile.sh
@@ -0,0 +1,3 @@
+for i in `find ./ -type f -name '*' ` ; do  cat $i |grep youzu ; done
+for i in `find ./ -type f -name '*' ` ; do  cat $i |grep Oi ; done
+for i in `find ./ -type f -name '*' ` ; do  cat $i |grep 44 |grep oi; done  
